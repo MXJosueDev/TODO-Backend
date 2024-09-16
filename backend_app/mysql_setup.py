88245a806ce72e421e0f63c8e9be388b8e8dda26
Josue L <69768362+MXJosueDev@users.noreply.github.com>
2024-09-15 18:58:54 -0600

    Added suppor for mysql

---
+++ backend_app/mysql_setup.py
@@ -0,0 +1,2 @@
+pymysql.version_info = (1, 4, 3, "final", 0)
+pymysql.install_as_MySQLdb()
--- backend_app/settings.py
+++ backend_app/settings.py
@@ -20,9 +20,6 @@ import dj_database_url
 
 load_dotenv()
 
-import pymysql
-pymysql.install_as_MySQLdb()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
