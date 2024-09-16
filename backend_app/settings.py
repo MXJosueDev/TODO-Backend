0c6c327a3a0a63df305c86f6c8a204422f8a443f
Josue L <69768362+MXJosueDev@users.noreply.github.com>
2024-09-15 18:49:07 -0600

    Pls

--- backend_app/settings.py
+++ backend_app/settings.py
@@ -65,7 +65,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware'
+    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
